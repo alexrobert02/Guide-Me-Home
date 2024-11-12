d81787f6f3ea37493599249dd781562bc8361e16
Robert-Alexandru Zaharia <alex_robert02@yahoo.com>
2024-11-12 21:14:45 +0200

    user security

---
+++ backend/src/main/java/com/guidemehome/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package com.guidemehome.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+        info = @Info(
+                description = "OpenApi documentation for Guide-Me-Home",
+                title = "OpenApi specification"
+        ),
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
