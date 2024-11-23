a4fa46317a04e340d61f9427e84aa76eccd98ca3
alin090402 <alin.sava02@gmail.com>
2024-11-23 17:18:42 +0200

    upgrade from dist to build

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.example.guidemehome",
   appName: "guide-me-home",
-  webDir: "dist",
+  webDir: "build",
 };
 
 export default config;
