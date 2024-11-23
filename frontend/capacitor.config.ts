5271ebf18e6f604f4d013e2026cd7ff008a433eb
alin090402 <alin.sava02@gmail.com>
2024-11-23 15:49:02 +0200

    temporary commit

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.example.guidemehome",
   appName: "guide-me-home",
-  webDir: "build",
+  webDir: "dist",
 };
 
 export default config;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
