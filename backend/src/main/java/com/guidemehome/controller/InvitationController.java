bf784baf3223f0a48d2fbc48b6501041835ae671
Dascălu Rareș <rares-vasilica.dascalu@s.unibuc.ro>
2024-12-17 18:48:28 +0200

    invitation mail

--- backend/src/main/java/com/guidemehome/controller/InvitationController.java
+++ backend/src/main/java/com/guidemehome/controller/InvitationController.java
@@ -1,4 +1,37 @@
 package com.guidemehome.controller;
 
+import com.guidemehome.configuration.PublicEndpoint;
+import com.guidemehome.dto.InvitationDto;
+import com.guidemehome.service.InvitationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/invitation")
 public class InvitationController {
+    @Autowired
+    private InvitationService invitationService;
+
+    @PublicEndpoint
+    @PostMapping
+    public ResponseEntity<String> sendInvitation(@RequestBody InvitationDto invitationDto) {
+        try {
+            invitationService.sendInvitationRequest(invitationDto.getSenderId(), invitationDto.getRecipientEmail());
+            return ResponseEntity.ok("Invitație trimisă!");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PublicEndpoint
+    @PostMapping("/accept")
+    public ResponseEntity<String> acceptInvitation(@RequestParam String token) {
+        try {
+            String message = invitationService.acceptInvitationRequest(token);
+            return ResponseEntity.ok(message);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
---
+++ backend/src/main/java/com/guidemehome/dto/InvitationDto.java
@@ -0,0 +1,11 @@
+package com.guidemehome.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InvitationDto {
+    private String senderId;
+    private String recipientEmail;
+}
