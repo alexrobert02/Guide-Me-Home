7a3a13f3ab720414468ae2276ae9907c0f307f78
cristian <you@example.com>
2025-01-09 20:50:16 +0200

    keys

--- frontend/src/ProjectDefaults.ts
+++ frontend/src/ProjectDefaults.ts
@@ -1,2 +1,2 @@
-export const DEFAULT_BACKEND_API_URL = "http://localhost:8080";
-// export const DEFAULT_BACKEND_API_URL = "http://192.168.100.6:8080";
+// export const DEFAULT_BACKEND_API_URL = "http://localhost:8080";
+export const DEFAULT_BACKEND_API_URL = "http://192.168.100.6:8080";
