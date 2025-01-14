3c8ec4465ec7db0fba1eaef68e3a7522db33afee
Robert-Alexandru Zaharia <alex_robert02@yahoo.com>
2025-01-14 19:54:42 +0200

    + diagrams

--- frontend/src/ProjectDefaults.ts
+++ frontend/src/ProjectDefaults.ts
@@ -1,4 +1,4 @@
-// export const DEFAULT_BACKEND_API_URL = "http://localhost:8080";
-
-export const DEFAULT_BACKEND_API_URL = "http://157.230.25.219:8080";
+//export const DEFAULT_BACKEND_API_URL = "http://localhost:8080";
+export const DEFAULT_BACKEND_API_URL = "http://192.168.238.64:8080";
+// export const DEFAULT_BACKEND_API_URL = "http://157.230.25.219:8080";
 //export const DEFAULT_BACKEND_API_URL = "http://192.168.1.5:8080";
