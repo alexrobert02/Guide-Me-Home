f732db111aa705c7f6097cc7d69edee50aaffb41
alin090402 <alin.sava02@gmail.com>
2025-01-19 20:34:57 +0200

    show beauty markers

--- frontend/src/map/components/MapWrapper.tsx
+++ frontend/src/map/components/MapWrapper.tsx
@@ -1,4 +1,4 @@
-import { AdvancedMarker, Map } from '@vis.gl/react-google-maps';
+import { AdvancedMarker, Map, Pin } from '@vis.gl/react-google-maps';
 import * as React from "react";
 import { observer } from "mobx-react"
 import { LocationStore } from '../../stores/LocationStore';
@@ -30,9 +30,27 @@ const MapWrapper = observer (() => {
               draggable={mapStore.draggable}
               onDrag={(e) => mapStore.onMarkerDrag(marker.id, e)}
               onDragEnd={(e) => mapStore.onMarkerDragEnd(marker.id, e)}
-              
               >
-
+                {
+                  mapStore.draggable ? null : (
+                    index === mapStore.markers.length - 1 ?
+                    (
+                      <Pin
+                        background='black'
+                        borderColor='black'
+                        glyphColor='white'
+                      />
+                    ) :
+                    (
+                      <Pin
+                        background='red'
+                        borderColor='red'
+                        glyphColor='white'
+                      />
+                    )
+                  )
+                }
+                
               </AdvancedMarker>
             )
           })
